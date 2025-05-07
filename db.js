770da4b98ec3db8d521d7b07a4c27615648ff397
yannisaktouf <yannis_aktouf@carrefour.com>
2025-05-07 15:16:26 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 // Si DATABASE_URL est défini (en prod sur Railway), on l’utilise,
 // sinon on tombe sur ta config locale.
 const connectionString = process.env.DATABASE_URL || 
-  'postgres://postgres:123456@localhost:5432/Workflow_Promo';
+  'postgresql://workflow_promo_user:3R4UXo5sAPn4Kr5maxplL9Otjj6VplHB@dpg-d0dlh22dbo4c738meg10-a.oregon-postgres.render.com/workflow_promo';
 
 const pool = new Pool({
   connectionString,
